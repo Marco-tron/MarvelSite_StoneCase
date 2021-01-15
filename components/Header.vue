fc2ebca62eaa1b8ecd0b8b0934ee923a23ab7e9b
Marco <marcosimoes1998@gmail.com>
2021-01-15 00:58:48 -0300

    syntax fix

--- components/Header.vue
+++ components/Header.vue
@@ -2,22 +2,22 @@
   <div>
         <header class="h-20 z-20 bg-gray-900 w-full text-white font-semibold flex justify-between text-xl">
             <div class="h-full flex justify-center">
-                <Clickable class="w-32 flex justify-center">
+                <clickable class="w-32 flex justify-center">
                     LOGIN
-                </Clickable>
+                </clickable>
                 <span class="mx-1 flex items-center">|</span>
-                <Clickable class="w-32 flex justify-center">
+                <clickable class="w-32 flex justify-center">
                     CADASTRO
-                </Clickable>
+                </clickable>
             </div>
       </header>
       <nav class="h-10 z-10 w-full bg-gray-600 text-white flex justify-around shadow-xl">
-            <Clickable class="w-32 flex justify-center">
+            <clickable class="w-32 flex justify-center">
                 COMICS
-            </Clickable>
-            <Clickable class="w-32 flex justify-center">
+            </clickable>
+            <clickable class="w-32 flex justify-center">
                 PERSONAGENS
-            </Clickable>
+            </clickable>
       </nav>
   </div>
 </template>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <MarvelHeader/>
+    <marvel-header/>
     <nuxt />
   </div>
 </template>
