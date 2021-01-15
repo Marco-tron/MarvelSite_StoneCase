56c8477e805687ad1f51ae2d2101dade509351f0
Marco <marcosimoes1998@gmail.com>
2021-01-15 11:58:10 -0300

    feature (navbar) navbar sending to correct link

--- components/Header.vue
+++ components/Header.vue
@@ -12,10 +12,10 @@
             </div>
       </header>
       <nav class="h-10 z-10 w-full bg-gray-600 text-white flex justify-around shadow-xl">
-            <clickable class="w-32 flex justify-center">
+            <clickable to="/comics" class="w-32 flex justify-center">
                 COMICS
             </clickable>
-            <clickable class="w-32 flex justify-center">
+            <clickable to="/personagens" class="w-32 flex justify-center">
                 PERSONAGENS
             </clickable>
       </nav>
