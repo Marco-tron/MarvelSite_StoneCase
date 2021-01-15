f97ed7551849c6db7b4d222d1420a3b96ffc7f57
Marco <marcosimoes1998@gmail.com>
2021-01-15 00:44:49 -0300

    feature (home) title on home

--- pages/index.vue
+++ pages/index.vue
@@ -1,30 +1,13 @@
 <template>
-  <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        MarvelSite_StoneCase
-      </h1>
-      <h2 class="subtitle">
-        site made in 7 days which lets you search and favorite Marvel&#39;s comics and characters
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
+  <div>
+        <div class="flex flex-col items-center text-center">
+            <h1 class="text-4xl text-gray-900 font-bold">
+                COMEÇE A PROCURAR
+            </h1>
+            <h2 class="text-xl text-gray-600">
+                Quer encontrar aquele quadrinho inesquecível? Ou o personagem que você ama?
+            </h2>
+        </div>
   </div>
 </template>
 
@@ -44,34 +27,4 @@ export default {
   @apply min-h-screen flex justify-center items-center text-center mx-auto;
 }
 */
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
 </style>
