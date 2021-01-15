74c1641e0d3f869bc8e58d8d14f96e2e787516b6
Marco <marcosimoes1998@gmail.com>
2021-01-15 00:19:59 -0300

    bugfix (header) fix identation

--- components/Header.vue
+++ components/Header.vue
@@ -1,15 +1,15 @@
 <template>
   <div>
-      <header class="h-20 z-20 bg-gray-900 w-full text-white font-semibold flex justify-between text-xl">
-          <div class="h-full flex justify-center">
-            <Clickable class="w-32 flex justify-center">
-                LOGIN
-            </Clickable>
-            <span class="mx-1 flex items-center">|</span>
-            <Clickable class="w-32 flex justify-center">
-                CADASTRO
-            </Clickable>
-          </div>
+        <header class="h-20 z-20 bg-gray-900 w-full text-white font-semibold flex justify-between text-xl">
+            <div class="h-full flex justify-center">
+                <Clickable class="w-32 flex justify-center">
+                    LOGIN
+                </Clickable>
+                <span class="mx-1 flex items-center">|</span>
+                <Clickable class="w-32 flex justify-center">
+                    CADASTRO
+                </Clickable>
+            </div>
       </header>
       <nav class="h-10 z-10 w-full bg-gray-600 text-white flex justify-around shadow-xl">
             <Clickable class="w-32 flex justify-center">
@@ -27,10 +27,10 @@ import Logo from '~/components/Logo.vue'
 import Clickable from '~/components/general/Clickable'
 
 export default {
-  components: {
-    Logo,
-    Clickable
-  }
+    components: {
+        Logo,
+        Clickable
+    }
 }
 </script>
 
