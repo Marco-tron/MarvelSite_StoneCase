2140d21f87a4d153a35700d954977eaa22b43bb1
Marco <marcosimoes1998@gmail.com>
2021-01-14 23:51:16 -0300

    repositioning code

--- components/general/Clickable.vue
+++ components/general/Clickable.vue
@@ -1,5 +1,5 @@
 <template>
-  <span class="hover:font-bold cursor-pointer h-full flex items-center border-b-4 border-transparent hover:border-red-800">
+  <span class="cursor-pointer h-full flex items-center border-b-4 border-transparent hover:border-red-800 hover:font-bold">
       <slot/>
   </span>
 </template>
