c330e5890d112717fd7074a817b2c1351169c70e
Marco <marcosimoes1998@gmail.com>
2021-01-15 15:32:48 -0300

    adding comments

--- pages/_category.vue/index.vue
+++ pages/_category.vue/index.vue
@@ -26,14 +26,19 @@ export default {
         $config
     }) {
         var md5 = require("md5"); 
+        // are you searching for comics or characters?
         const category = params.category.toUpperCase();
-        const privatekey = $config.privatekey;
-        const publickey = $config.publickey;
+
+        // Marvel's API  private and public key
+        const privatekey = $config.privateKey;
+        const publickey = $config.publicKey;
         const ts = new Date().valueOf();
+        // validations for Marvel's API
         const hash = md5(ts + privatekey + publickey);
-        console.log($config);
+
+        let response = [];
         try {
-            const items = await $axios.$get(`https://gateway.marvel.com:443/v1/public/characters?ts=${ts}&apikey=${publickey}&hash=${hash}`);
+            response = await $axios.$get(`https://gateway.marvel.com:443/v1/public/characters?ts=${ts}&apikey=${publickey}&hash=${hash}`);
             console.log(items);
         } catch (e) {
             console.log(e);
