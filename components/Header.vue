5e1287d366f1e4ea1addc3d45d0ed8afe309269b
Marco <marcosimoes1998@gmail.com>
2021-01-14 23:29:56 -0300

    feature (header) basic login layout

--- components/Header.vue
+++ components/Header.vue
@@ -1,18 +1,15 @@
 <template>
   <div>
-      <header class="h-20 bg-black w-full text-white font-semibold">
-          <div>
-            <span>
+      <header class="h-20 bg-gray-900 w-full text-white font-semibold flex justify-between text-xl">
+          <div class="h-full  flex justify-center">
+            <Clickable class="w-32 flex justify-center">
                 LOGIN
-            </span>
-            <span class="mx-1">
-                |
-            </span>
-            <span>
+            </Clickable>
+            <span class="mx-1 flex items-center">|</span>
+            <Clickable class="w-32 flex justify-center">
                 CADASTRO
-            </span>
+            </Clickable>
           </div>
-          
       </header>
       <nav>
 
@@ -22,10 +19,12 @@
 
 <script>
 import Logo from '~/components/Logo.vue'
+import Clickable from '~/components/general/Clickable'
 
 export default {
   components: {
-    Logo
+    Logo,
+    Clickable
   }
 }
 </script>
