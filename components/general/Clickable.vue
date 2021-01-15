2bfa34747eb3b7a68c05b1f4345f35e30fcd1863
Marco <marcosimoes1998@gmail.com>
2021-01-15 11:57:34 -0300

    feature (clickable) clickable component accepting link

--- components/general/Clickable.vue
+++ components/general/Clickable.vue
@@ -1,13 +1,20 @@
 <template>
-  <span class="cursor-pointer h-full flex items-center border-b-4 border-transparent hover:border-red-800 hover:font-bold">
-      <slot/>
-  </span>
+    <span class="cursor-pointer h-full flex items-center border-b-4 border-transparent hover:border-red-800 hover:font-bold">
+        <a :href="link">
+            <slot/>
+        </a>
+    </span>
 </template>
 
 <script>
 
 export default {
-
+    props: {
+        link: {
+            type: String,
+            default: '#'
+        },
+    }
 }
 </script>
 
