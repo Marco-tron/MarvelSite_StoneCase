7d0310de75ef8d80384363348fa8fcc69aceeb7d
Marco <marcosimoes1998@gmail.com>
2021-01-15 02:12:48 -0300

    feature (HomeItem) image size and layout

--- components/HomeItem.vue
+++ components/HomeItem.vue
@@ -1,10 +1,10 @@
 <template>
-    <a href="/comics" class="flex flex-col items-center ">
+    <a href="/comics" class="flex flex-col items-center">
         <h3 class="text-2xl font-semibold">
             COMICS
         </h3>
-        <div class="image">
-            <img src="" alt="">
+        <div class="image my-4">
+            <img class="object-cover h-full" src="~static/images/marvel_comics.jpg" alt="">
         </div>
         <div class="bg-gray-900 text-white  rounded-tl-lg rounded-br-lg">
             <clickable class="px-4 py-3">
@@ -26,7 +26,7 @@ export default {
 
 <style scoped>
     .image{
-        width: 400px;
-        height: 400px;
+        width: 480px;
+        height: 480px;
     }
 </style>
