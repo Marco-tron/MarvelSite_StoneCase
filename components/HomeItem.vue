bb18ae4f4e9c40ebb05331352f1d0e7e99bf76f4
Marco <marcosimoes1998@gmail.com>
2021-01-15 01:29:36 -0300

    feature (HomeItem) see all button

--- components/HomeItem.vue
+++ components/HomeItem.vue
@@ -1,19 +1,23 @@
 <template>
-    <div>
-        <h3>
+    <div class="flex flex-col items-center ">
+        <h3 class="text-2xl font-semibold">
             COMICS
         </h3>
         <img src="" alt="">
-        <button>
-            VER TODOS
-        </button>
+        <div class="bg-gray-900 text-white  rounded-tl-lg rounded-br-lg hover:rounded-none">
+            <clickable class="px-4 py-3">
+                VER TODOS
+            </clickable>
+        </div>
     </div>
 </template>
 
 <script>
+import Clickable from './general/clickable.vue'
 
 export default {
     components: {
+        Clickable
     }
 }
 </script>
