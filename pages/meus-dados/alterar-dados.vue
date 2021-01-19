7670c0d43e5bb6a821f4c35994990ca8ba089ba3
Marco Aurelio <marcosimoes1998@gmail.com>
2021-01-19 09:18:33 -0300

    feat: change data and password screens

---
+++ pages/meus-dados/alterar-dados.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="container">
+        <Title text="Alterar dados"/>
+        <Subtitle text="digite aqui o email e o nome novos que você deseja, espaços em branco não serão alterados" />
+        <div class="grid grid-cols-2 pb-48">
+            <div>
+                <div class="flex">
+                    <span class="font-bold">Nome:</span> <input v-model="user.name" type="text">
+                </div>
+                <div class="flex grid grid-cols-3">
+                    <span class="font-bold">E-mail:</span> <input v-model="user.email" type="text">
+                </div>
+            </div>
+        </div>
+        
+  </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+        user: {
+                name: "Marco",
+                email: "teste@teste.com"
+            }
+        }
+    }
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ pages/meus-dados/alterar-senha.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="container">
+        <Title text="Alterar Senha"/>
+        <Subtitle text="digite sua senha antiga e sua nova senha abaixo" />
+        <div class="grid grid-cols-2 pb-48">
+            <div>
+                <div class="flex">
+                    <span class="font-bold">Senha antiga:</span> <input v-model="user.oldPassword" type="password">
+                </div>
+                <div class="flex grid grid-cols-3">
+                    <span class="font-bold">Senha nova:</span> <input v-model="user.newPassword" type="password">
+                </div>
+            </div>
+        </div>
+        
+  </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+        user: {
+                oldPassword: "Marco",
+                newPassword: "teste@teste.com"
+            }
+        }
+    }
+
+}
+</script>
+
+<style>
+
+</style>
