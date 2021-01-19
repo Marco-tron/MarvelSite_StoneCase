a05d38e352841c965abf9378249d0d9cd1bb556d
Marco Aurelio <marcosimoes1998@gmail.com>
2021-01-19 09:08:19 -0300

    style: adjusting tabs

--- pages/meus-dados/index.vue
+++ pages/meus-dados/index.vue
@@ -4,16 +4,16 @@
         <Subtitle text="Aqui você pode ver e editar os dados de sua conta" />
         <div class="grid grid-cols-2 pb-48">
             <div>
-            <div class="flex grid grid-cols-3">
-                <span class="font-bold">Nome:</span> <span> {{user.name}} </span> <clickable class="w-32" link="/meusdados/alterar-dados"> Alterar dados</clickable>
+                <div class="flex grid grid-cols-3">
+                    <span class="font-bold">Nome:</span> <span> {{user.name}} </span> <clickable class="w-32" link="/meusdados/alterar-dados"> Alterar dados</clickable>
+                </div>
+                <div class="flex grid grid-cols-3">
+                    <span class="font-bold">E-mail:</span> <span> {{user.email}} </span> <clickable class="w-32" link="/meusdados/alterar-dados"> Alterar dados</clickable>
+                </div>
+                <div class="flex grid grid-cols-3">
+                    <span class="font-bold">Senha:</span> <span> ********* </span> <clickable class="w-32" link="/meusdados/alterar-senha"> Alterar senha</clickable>
+                </div>
             </div>
-            <div class="flex grid grid-cols-3">
-                <span class="font-bold">E-mail:</span> <span> {{user.email}} </span> <clickable class="w-32" link="/meusdados/alterar-dados"> Alterar dados</clickable>
-            </div>
-            <div class="flex grid grid-cols-3">
-                <span class="font-bold">Senha:</span> <span> ********* </span> <clickable class="w-32" link="/meusdados/alterar-senha"> Alterar senha</clickable>
-            </div>
-        </div>
         </div>
         
   </div>
