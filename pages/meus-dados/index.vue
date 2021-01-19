f356e91dcf3765d475655008e3c86629cedf9c08
Marco Aurelio <marcosimoes1998@gmail.com>
2021-01-19 09:15:29 -0300

    fix: correct link

--- pages/meus-dados/index.vue
+++ pages/meus-dados/index.vue
@@ -5,13 +5,13 @@
         <div class="grid grid-cols-2 pb-48">
             <div>
                 <div class="flex grid grid-cols-3">
-                    <span class="font-bold">Nome:</span> <span> {{user.name}} </span> <clickable class="w-32" link="/meusdados/alterar-dados"> Alterar dados</clickable>
+                    <span class="font-bold">Nome:</span> <span> {{user.name}} </span> <clickable class="w-32" link="/meus-dados/alterar-dados"> Alterar dados</clickable>
                 </div>
                 <div class="flex grid grid-cols-3">
-                    <span class="font-bold">E-mail:</span> <span> {{user.email}} </span> <clickable class="w-32" link="/meusdados/alterar-dados"> Alterar dados</clickable>
+                    <span class="font-bold">E-mail:</span> <span> {{user.email}} </span> <clickable class="w-32" link="/meus-dados/alterar-dados"> Alterar dados</clickable>
                 </div>
                 <div class="flex grid grid-cols-3">
-                    <span class="font-bold">Senha:</span> <span> ********* </span> <clickable class="w-32" link="/meusdados/alterar-senha"> Alterar senha</clickable>
+                    <span class="font-bold">Senha:</span> <span> ********* </span> <clickable class="w-32" link="/meus-dados/alterar-senha"> Alterar senha</clickable>
                 </div>
             </div>
         </div>
