802f60e9a0dd30104f9634474c0d474e29a7e941
Marco Aurelio <marcosimoes1998@gmail.com>
2021-01-19 00:28:02 -0300

    feat: login and sigin page

---
+++ components/UserInput.vue
@@ -0,0 +1,54 @@
+<template>
+    <form class="">
+        <Title :text="title" />
+        <Subtitle :text="subtitle" />
+        <div v-for="item in items" :key="item.index" class="my-3" >
+            {{item.name}}
+            <span><input v-model="item.model" class="px-1 ml-1 border-2 border-gray-600 rounded-tl-lg rounded-br-lg" type="text"></span>
+        </div>
+        <button @click.prevent.stop="SendData()">
+            {{action}}
+        </button>
+    </form>
+</template>
+
+<script>
+export default {
+    props: {
+        title: {
+            type: String,
+            default: ""
+        },
+        subtitle: {
+            type: String,
+            default: ""
+        },
+        action: {
+            type: String,
+            default: ""
+        }
+    },
+    data() {
+        return {
+            items: [
+            {
+                name: "E-mail",
+                model: ""
+            },
+            {
+                name: "Senha",
+                model: ""
+            }
+        ]
+        }
+    },
+    methods: {
+        
+    },
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ pages/login.vue
@@ -0,0 +1,54 @@
+<template> 
+    <div class="container grid grid-cols-2">
+        <div>
+            <user-input title="LOGIN" subtitle="Entre com seu email e senha" action="login"/>
+            <div class="flex text-gray-600 text-2xl justify-between items-center">
+                <span class="h-1 w-1/2 bg-gray-600" />
+                <span class="mx-4">
+                    ou
+                </span>
+                <span class="h-1 w-1/2 bg-gray-600" />
+            </div>
+            <user-input title="CADASTRO" subtitle="Cadastre um email e senha para utilizar mais funcionalidades de nosso site" action="cadastre-se"/>
+
+        </div>
+    </div>
+</template>
+
+<script>
+import UserInput from "~/components/UserInput.vue";
+export default {
+    components: {
+        UserInput
+    },
+    data() {
+        return {
+            login: [
+                {
+                    name: "E-mail",
+                    model: ""
+                },
+                {
+                    name: "Senha",
+                    model: ""
+                }
+            ],
+            signUp: [
+                {
+                    name: "E-mail",
+                    model: ""
+                },
+                {
+                    name: "Senha",
+                    model: ""
+                }
+            ]
+        }
+    }
+
+}
+</script>
+
+<style>
+
+</style>
