735d5890eba801a773a94781c4f9d0a2e9051a62
Marco Aurelio <marcosimoes1998@gmail.com>
2021-01-19 01:21:25 -0300

    feat: user data page

---
+++ pages/meus-dados/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="container">
+      <Title text="MEUS DADOS"/>
+      <Subtitle text="Aqui você pode ver e editar os dados de sua conta" />
+      <div>
+            <div>
+                Nome: <span> {{user.name}} </span>
+            </div>
+            <div>
+                email: <span> {{user.email}} </span>
+            </div>
+      </div>
+      <div>
+          Senha: <span> ********* </span>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            user: {
+                name: "Marco",
+                email: "teste@teste.com"
+            }
+        }
+    }
+
+}
+</script>
+
+<style>
+
+</style>
