0aa8d9bbfadf6c088a248560b9fe1b7e9810257c
Marco <marcosimoes1998@gmail.com>
2021-01-20 18:52:35 -0300

    feat: change password available

--- pages/meus-dados/alterar-senha.vue
+++ pages/meus-dados/alterar-senha.vue
@@ -1,32 +1,62 @@
 <template>
-    <div class="container">
+    <div v-if="$store.state.loggedIn" class="container pb-56">
         <Title text="Alterar Senha"/>
         <Subtitle text="digite sua senha antiga e sua nova senha abaixo" />
         <div class="grid grid-cols-2 pb-48">
-            <div>
-                <div class="flex">
-                    <span class="font-bold">Senha antiga:</span> <input v-model="user.oldPassword" type="password">
+            <form>
+                <div class="grid grid-cols-3 my-2">
+                    <span class="font-bold mr-2">Senha antiga:</span> <input class="input" v-model="user.oldPassword" type="text">
                 </div>
-                <div class="flex grid grid-cols-3">
-                    <span class="font-bold">Senha nova:</span> <input v-model="user.newPassword" type="password">
+                <div class="grid grid-cols-3 my-2">
+                    <span class="font-bold mr-2">Senha nova:</span> <input class="input" v-model="user.password" type="text">
                 </div>
-            </div>
+                <button class="button" @click.prevent.stop="attData()">
+                    ATUALIZAR
+                </button>
+                <div v-if="message.show" :class="message.class">
+                    {{message.text}}
+                </div>
+            </form>
+            
         </div>
         
-  </div>
+    </div>
 </template>
 
 <script>
 export default {
     data() {
         return {
-        user: {
-                oldPassword: "Marco",
-                newPassword: "teste@teste.com"
+            user: {
+                oldPassword: "",
+                password: ""
+            },
+            message: {
+            }
+        }
+    },
+    methods: {
+        async attData() {
+            try {
+                const response = await this.$axios.$put(`${this.$config.host}/users/newPassword`, this.user, {
+                    headers: { authorization: this.$store.state.token}
+                });
+                console.log(response);
+                this.message =  {
+                    text:"Senha atualizado com sucesso",
+                    class: "text-blue-500",
+                    show: true
+                }
+            } catch(e){
+                this.message =  {
+                    text:"Houve um problema ao atualizar apague e tente novamente",
+                    class: "text-red-500",
+                    show: true
+                }
+                console.log(e)
             }
         }
     }
-
 }
 </script>
 
