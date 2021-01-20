22fddea12450e4fda8d2fce67e10af9a5ec9f87e
Marco <marcosimoes1998@gmail.com>
2021-01-20 15:48:37 -0300

    feat: showing user data

--- pages/meus-dados/index.vue
+++ pages/meus-dados/index.vue
@@ -1,14 +1,14 @@
 <template>
-    <div class="container">
+    <div v-if="$store.state.loggedIn" class="container">
         <Title text="MEUS DADOS"/>
         <Subtitle text="Aqui você pode ver e editar os dados de sua conta" />
         <div class="grid grid-cols-2 pb-48">
             <div>
                 <div class="flex grid grid-cols-3">
-                    <span class="font-bold">Nome:</span> <span> {{user.name}} </span> <clickable class="w-32" link="/meus-dados/alterar-dados"> Alterar dados</clickable>
+                    <span class="font-bold">Nome:</span> <span> {{$store.state.user.name}} </span> <clickable class="w-32" link="/meus-dados/alterar-dados"> Alterar dados</clickable>
                 </div>
                 <div class="flex grid grid-cols-3">
-                    <span class="font-bold">E-mail:</span> <span> {{user.email}} </span> <clickable class="w-32" link="/meus-dados/alterar-dados"> Alterar dados</clickable>
+                    <span class="font-bold">E-mail:</span> <span> {{$store.state.user.email}} </span> <clickable class="w-32" link="/meus-dados/alterar-dados"> Alterar dados</clickable>
                 </div>
                 <div class="flex grid grid-cols-3">
                     <span class="font-bold">Senha:</span> <span> ********* </span> <clickable class="w-32" link="/meus-dados/alterar-senha"> Alterar senha</clickable>
